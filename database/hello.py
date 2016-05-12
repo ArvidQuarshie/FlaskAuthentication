ee71a98466367123403e9cc015d368c0a7f94c30
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-12 08:18:11 +0300

    hello.py

---
+++ database/hello.py
@@ -0,0 +1,9 @@
+#database configuration
+
+from flask.ext.sqlalchemy import SQLAlchemy
+basedir =os.path.abspath(os.path.dirname(__file__))
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    sqlite:///+os.path.join(basedir,'data.sqlite')
+app.config['SQLAlCHEMY_COMMIT_ON_TEARDOWN'] = True
+db=SQLAlchemy(app)
