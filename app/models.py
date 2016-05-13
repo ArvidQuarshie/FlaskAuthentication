b00c0bb383340eddb5c8c3c2b0f3e817c2fdb086
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-13 08:19:51 +0300

    models .py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    #...
+    password_hash=db.Column(db.String(128))
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute")
+    @password.setter(self,password):
+        self.password_hash=generate_password_hash(password)
+    def verify_password(self,password):
+        self.password_hash=generate_password_hash(password)
+        
+
+
+
 from flask.ext.login import UserMixin
 
 
