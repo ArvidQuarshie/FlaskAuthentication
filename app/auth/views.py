86717b364cd74f58718c1d3d8ca42fcae510fe39
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-16 10:32:44 +0300

    authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,28 @@
+# imports the blueprint and defines routes associated 
+ #with authentication using a route decorator"""
+
 from flask import render_template
 from . import auth
 
 @auth.route(./login)
 def login():
     return render_template('auth/login.html')
+
+#sign in route
+
+from flask import render_template,redirect,request,url_for,flash
+from flask.ext.login import login_user
+from . import auth
+from ..models import User
+from .forms import LoginForm
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data)
+        login_user(user,form.remember_me.data)
+        return redirect(request.args.get('next') or url_for('main.index'))
+    flash('Invalid username or password')
+
