3cfdf37b337ddcc2dfefa8e826e2c70b35ea9f30
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-16 11:07:14 +0300

    authentication

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,13 @@
 from flask.ext.wtf import Form
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import Required,Email
+from wtforms.validators import Required,Email,Regexp,EqualTo
+from wtforms import ValidationError
+from ..models import User
 
-class LoginForm(Form)
+class RegistrationForm(Form)
     email=StringField('Email',validators=[Required(),Length(1,64),Email()])
-    password=PasswordField('Password',validators=[Required()])
-    submit=SubmitField('Log In')
+    username=StringField('Username', validators=[Required(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+'Usernames must have only letters, '
+'numbers, dots or underscores')])
+    password=PasswordFiels('password', validators=[Required(), EqualTo('password2',message='Passwords must match')])
+   
